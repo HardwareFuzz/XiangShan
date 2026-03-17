1038220f8babd3cbff17a9d1b975fc2adaa359ef
canxin121 <q1969730106@gmail.com>
2026-03-18 01:15:35 +0800

    build: make --clean clean+build

--- build.sh
+++ build.sh
@@ -156,7 +156,6 @@ workdir="$BUILD_ROOT/.work/${name_base}${cov_suffix}"
 if [[ $DO_CLEAN -eq 1 ]]; then
   rm -rf "$workdir" "$artifact"
   echo "cleaned: $artifact"
-  exit 0
 fi
 
 mkdir -p "$BUILD_ROOT" "$OUT_DIR"
